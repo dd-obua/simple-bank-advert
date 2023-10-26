014a1212c0892772861eadb7095ee7dbfdf04134
Obua Denis Daniel <dd.obua@gmail.com>
2023-10-26 19:19:03 +0300

    Include markup and styles

---
+++ js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Select elements
+const select = (selector) => document.querySelector(selector);
+const selectAll = (selector) => document.querySelectorAll(selector);
+
+// Modal window
+const modal = select('.modal');
+const overlay = select('.overlay');
+const btnCloseModal = select('.btn--close-modal');
+const btnsOpenModal = selectAll('.btn--show-modal');
+
+const openModal = (event) => {
+  event.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener('click', openModal));
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (event) => {
+  event.key === 'Escape' && !modal.classList.contains('hidden') && closeModal();
+});
