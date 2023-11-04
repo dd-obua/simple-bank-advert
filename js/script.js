441df8a51edeb25ed12a1f0d22d1b4e1cee82120
Obua Denis Daniel <dd.obua@gmail.com>
2023-11-04 12:13:34 +0300

    Remove bind implementation

--- js/script.js
+++ js/script.js
@@ -67,18 +67,19 @@ tabsContainer.addEventListener('click', (event) => {
 });
 
 // Menu fade animation
-const handleEvent = function (event, opacity) {
+const handleEvent = (event) => {
   if (event.target.classList.contains('nav__link')) {
     const link = event.target;
     const siblings = link.closest('.nav__links').querySelectorAll('.nav__link');
     const logo = link.closest('.nav').querySelector('img');
 
+    const value = event.type === 'mouseover' ? 0.3 : 1;
     siblings.forEach((sibling) => {
-      if (sibling !== link) sibling.style.opacity = this;
+      if (sibling !== link) sibling.style.opacity = value;
     });
-    logo.style.opacity = this;
+    logo.style.opacity = value;
   }
 };
 
-nav.addEventListener('mouseover', handleEvent.bind(0.3));
-nav.addEventListener('mouseout', handleEvent.bind(1));
+nav.addEventListener('mouseover', handleEvent);
+nav.addEventListener('mouseout', handleEvent);
